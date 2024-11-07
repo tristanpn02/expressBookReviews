69b4a0ad07b919f77eeeb43964bff969b27fdb3d
Tristan Andersen <tpan02@pm.me>
2024-11-07 02:05:17 +0000

    Add a review found on amazon.nl since no reviews are provided in booksdb.js

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,5 +1,10 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
+      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {
+            "reviewer": "B.E",
+            "title": "A Classic",
+            "body": "Just buy it already and read. Then gift to your best friend",
+            "source": "Review found on Amazon.nl since booksdb.js doesn't include any reviews."
+      } },
       2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
       3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
       4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
