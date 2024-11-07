2dd74c5bfab6b10fa4fcf780ab9e240a6ed97643
Tristan Andersen <tpan02@pm.me>
2024-11-07 01:41:13 +0000

    Change array name to be more descriptive

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -36,20 +36,20 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   let author = req.params.author;
-  let authorsBooks = []; // Array to be populated with books by author
+  let booksByAuthor = []; // Array to be populated with books by author
 
   // Return array of books by author provided if they exist
   if (author) {
     // Iterate through books array, check if author matches the one provided and add to authorsBooks
     Object.keys(books).forEach((key, value) => {
       if (books[key].author === author) {
-        authorsBooks.push(books[key]);
+        booksByAuthor.push(books[key]);
       }
     })
 
-    // Check if books have been added to authorsBooks array
-    if (authorsBooks.length > 0) {
-      return res.send(JSON.stringify(authorsBooks, null, 4));
+    // Check if books have been added to booksByAuthor array
+    if (booksByAuthor.length > 0) {
+      return res.send(JSON.stringify(booksByAuthor, null, 4));
     } else {
       return res.status(404).json({message: `No books by author ${author} found.`})
     }
